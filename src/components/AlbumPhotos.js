30c30ddac944819a1f9c6148150b901aff5b9956
Andi Hakim <andi.hakim.arif@gmail.com>
2019-03-03 21:33:08 +0700

    feature view photo detail

--- src/components/AlbumPhotos.js
+++ src/components/AlbumPhotos.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { List, Alert } from "antd";
+import { Card } from "antd";
 
-import { List, Avatar, Alert } from "antd";
-import Logo from "../logo.svg";
+const { Meta } = Card;
+
+const Photo = ({ title, thumbnailUrl }) => (
+  <Card
+    hoverable="true"
+    cover={<img alt={title} src={thumbnailUrl} style={{ height: "25vh" }} />}
+  >
+    <Meta title={title} description={title} style={{ height: "60px" }} />
+  </Card>
+);
 
 const AlbumPhotos = ({ data, loading, error }) =>
   error ? (
@@ -11,16 +20,14 @@ const AlbumPhotos = ({ data, loading, error }) =>
   ) : (
     <List
       itemLayout="horizontal"
+      size="small"
       dataSource={data}
       loading={loading}
-      pagination="true"
+      pagination={{ pageSize: 8 }}
+      grid={{ gutter: 16, column: 4 }}
       renderItem={item => (
         <List.Item>
-          <List.Item.Meta
-            avatar={<Avatar src={Logo} />}
-            title={<Link to="#">{item.title}</Link>}
-            description={item.body}
-          />
+          <Photo {...item} />
         </List.Item>
       )}
     />
